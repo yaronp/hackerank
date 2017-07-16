39e9f46be4feb6005b8808ef0de8446baa514362
Yaron Pdut <yaron.pdut@nice.com>
2017-07-16 11:28:56 +0300

    submited

--- challenges/breaking-best-and-worst-records/breaking_best_and_worst_records.py
+++ challenges/breaking-best-and-worst-records/breaking_best_and_worst_records.py
@@ -1,13 +1,26 @@
 #!/bin/python
+import math
 
-import sys
 
+def get_record(s):
+    scores = map(int, s.strip().split(' '))
+    max_score = min_score = scores[0]
+    min_count, max_count = 0, 0
 
-def get_record(n, s):
-    return s
+    i = 0
+    for j in range(len(scores)-1):
+        i = i + 1
+        if scores[j+1] > max_score:
+            max_count = max_count + 1
+            max_score = scores[j+1]
+        if scores[j+1] < min_score:
+            min_count = min_count + 1
+            min_score = scores[j+1]
+    return [str(min_count), str(max_count)]
+
 
 if __name__ == '__main__':
     n = int(raw_input().strip())
     s = map(int, raw_input().strip().split(' '))
-    result = get_record(n, s)
-    print " ".join(map(str, result))
+    result = get_record(s)
+    print " ".join(result)
--- challenges/breaking-best-and-worst-records/unit_test.py
+++ challenges/breaking-best-and-worst-records/unit_test.py
@@ -5,10 +5,14 @@ from breaking_best_and_worst_records import get_record
 
 class TestBreakingBest(unittest.TestCase):
     def test_case1(self):
-        n = 9
         s = "10 5 20 20 4 5 2 25 1"
-        res = get_record(n, s)
-        self.assertEqual(res, [4, 0])
+        res = get_record(map(int, s.strip().split(' ')))
+        self.assertEqual(res, [4, 2])
+
+    def test_case2(self):
+        s = "3 4 21 36 10 28 35 5 24 42"
+        res = get_record(map(int, s.strip().split(' ')))
+        self.assertEqual(res, [4, 2])
 
 
 if __name__ == '__main__':
