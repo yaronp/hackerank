ebb2b6c1ea6bc2b4404d4599678de53b0b23968e
Yaron Pdut <yaron.pdut@nice.com>
2017-08-07 12:41:55 +0300

    solved

--- challenges/arrays-ds/src/main/java/com/yaronp/Solution.java
+++ challenges/arrays-ds/src/main/java/com/yaronp/Solution.java
@@ -1,19 +1,25 @@
 package com.yaronp;
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class Solution {
+
+    public static int[] reverseArray(int arr[], int n) {
+        int new_arr[] = new int[n];
+        for (int i = 0; i < n; i++)
+            new_arr[n-i-1] = arr[i];
+        return new_arr;
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int arr[] = new int[n];
-        for(int arr_i=n-1; arr_i > 0; arr_i--){
+        for (int arr_i = 0; arr_i < n; arr_i++) {
             arr[arr_i] = in.nextInt();
         }
+        int[] na = reverseArray(arr, n);
+        for (int i = n; i > 0; i--)
+            System.out.printf("%d%s", na[i - 1], (i == 0 ? "" : " "));
     }
 }
--- challenges/arrays-ds/src/test/java/com/yaronp/AppTest.java
+++ challenges/arrays-ds/src/test/java/com/yaronp/AppTest.java
@@ -4,35 +4,37 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.Arrays;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        int n = 4;
+        int[] a = { 1, 4, 3, 2 };
+        int[] na = Solution.reverseArray(a, n);
+        int[] nar = {2, 3, 4, 1};
+        assertTrue(Arrays.equals(na, nar));
     }
 }
